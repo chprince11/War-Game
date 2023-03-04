05669e9f904300cebb540f29426e4f7d12ed3a6a
LazyPencils <yihanglin@yahoo.ca>
2023-03-04 13:59:35 -0500

    Add name

--- src/ca/sheridancollege/project/Card.java
+++ src/ca/sheridancollege/project/Card.java
@@ -1,6 +1,6 @@
 /**
  * SYST 17796 Project Base code.
- * Students can modify and extend to implement their game.
+ * @author LazyPencils (Yihang Lin)
  * Add your name as an author and the date!
  */
 package ca.sheridancollege.project;
