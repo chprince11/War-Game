699f50741838987bd037f056ad8a514f29aa8f69
chprince11 <122410008+chprince11@users.noreply.github.com>
2023-03-06 18:23:49 -0500

    Update Deck.java

--- src/ca/sheridancollege/project/Deck.java
+++ src/ca/sheridancollege/project/Deck.java
@@ -1,21 +1,16 @@
 /**
- * SYST 17796 Project Base code.
- * Students can modify and extend to implement their game.
- * Add your name as an author and the date!
+ * SYST 17796 War Game project base code.
+ * 
+ * @author chprince11 (Prince Chauhan) 06 Feb 2023
+ * @author LazyPencils (Yihang Lin) 06 Feb 2023
+ * @author balkaran10 (Balkaran Singh) 06 Feb 2023
+ * @author Haimanyu (Haimanyu Haimanyu) 06 Feb 2023
  */
+
 package ca.sheridancollege.project;
 
 import java.util.ArrayList;
 import java.util.Collections;
-
-/**
- * A concrete class that represents any grouping of cards for a Game. HINT, you might want to subclass this more than
- * once. The group of cards has a maximum size attribute which is flexible for reuse.
- *
- * @author dancye
- * @author Paul Bonenfant Jan 2020
- */
-
 
 public class Deck 
 {
